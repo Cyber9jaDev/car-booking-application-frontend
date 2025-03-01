ccd403ced6a8f6d3bc461ee6efc190a4bf0912d9
cyber9ja <ayodejioladapo15@gmail.com>
2025-03-01 21:46:09 +0000

    Improved auth type definitiions and added login and signup pages

--- src/types/auth.types.ts
+++ src/types/auth.types.ts
@@ -1,33 +1,35 @@
 export type Role = 'PASSENGER' | 'ADMIN';
 
-export interface AuthForm {
-  name: string;
-  email: string;
-  phone_number: string;
-  password: string;
-  confirm_password: string;
-  terms_and_conditions: boolean;
-  errors?:{
-    name?: string[] | undefined;
-    email?: string[] | undefined;
-    phone_number?: string[] | undefined;
-    password?: string[] | undefined;
-    confirm_password?: string[] | undefined;
-  }
-}
-export const initialAuthFormState: AuthForm = {
-  name: "",
-  email: "",
-  phone_number: "",
-  password: "",
-  confirm_password: "",
-  terms_and_conditions: false,
-  // errors: {
-  //   name: undefined,
-  //   email: undefined,
-  //   phone_number: undefined,
-  //   password: undefined,
-  //   confirm_password: undefined,
-  // }
+interface FormFieldErrors {
+  [key: string]: string[] | undefined;
 }
 
+interface BaseAuthForm {
+  email: string;
+  password: string;
+  errors?: FormFieldErrors;
+}
+
+export interface LoginAuthForm extends BaseAuthForm {}
+
+export interface RegisterAuthForm extends BaseAuthForm {
+  name: string;
+  phoneNumber: string;
+  confirmPassword: string;
+  hasAgreedTermsAndConditions: boolean;
+}
+
+export const initialRegisterFormState: RegisterAuthForm = {
+  name: "",
+  email: "",
+  phoneNumber: "",
+  password: "",
+  confirmPassword: "",
+  hasAgreedTermsAndConditions: false,
+}
+
+
+export const initialLoginFormState: BaseAuthForm = {
+  email: "",
+  password: "",
+}
