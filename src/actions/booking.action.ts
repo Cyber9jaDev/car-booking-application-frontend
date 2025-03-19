9af4639a4e6c475e235761c9d1647c1eb32497a3
cyber9ja <ayodejioladapo15@gmail.com>
2025-03-19 15:19:36 +0000

    ...

--- src/actions/booking.action.ts
+++ src/actions/booking.action.ts
@@ -3,16 +3,14 @@
 import { BookingForm } from "@/interface/booking.interface";
 import { BookingFormSchema } from "@/lib/zod";
 import { City } from "@/utils/constants";
-// import { redirect } from "next/navigation";
 
 export async function getBookings( previousState: BookingForm, formData: FormData ) {
-  
+
   const validatedFields = BookingFormSchema.safeParse({
     arrivalCity: formData.get("arrivalCity"),
     departureCity: formData.get("departureCity"),
     departureDate: formData.get("departureDate"),
   });
-
 
   if (!validatedFields.success) {
     return {
@@ -23,14 +21,10 @@ export async function getBookings( previousState: BookingForm, formData: FormDat
     };
   }
 
-  console.table(validatedFields.data);
-  console.log(validatedFields.data);
-
   return {
     arrivalCity: formData.get("arrivalCity") as City,
     departureCity: formData.get("departureCity") as City,
     departureDate: formData.get("departureDate") as string,
   };
 
-  // redirect("/");
 }
